1c779e532e3fb154233e2a28c35c02cc4d708c08
Jennifer Silva <jennifer.silva@bandtec.com.br>
2020-06-17 22:40:20 -0300

    Upando arquivos do repositório local

---
+++ bdDesafio.sql
@@ -0,0 +1,10 @@
+create database DesafioIndividual;
+use DesafioIndividual;
+
+create table tbLogin(
+	id int primary key auto_increment,
+    Login varchar(10),
+    Senha varchar(20)
+    );
+
+select * from tbLogin;
